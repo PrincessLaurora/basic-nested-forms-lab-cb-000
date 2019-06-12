27245d8d8179c11b1af0eeb0baed894b37d47b89
Laura Felce <crepu83@yahoo.it>
2019-06-12 07:46:29 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @person.ingredients.build(ingredient_type: 'name')
+    @person.ingredients.build(ingredient_type: '0')
   end
 
   def create
